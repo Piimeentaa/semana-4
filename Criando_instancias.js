ce9937b7e4e74595ce03b5c9cf6964a4e38a21df
Piimeentaa <dora13.isa@gmail.com>
2019-10-11 21:42:40 -0300

    Instancias

---
+++ Criando_instancias.js
@@ -0,0 +1,8 @@
+function Gato(nome,sexo,cor,idade){
+    this.nome = nome;
+    this.sexo = sexo;
+    this.cor = cor;
+    this.idade = idade;
+}
+const satanas = new Gato('Satanas','masculino','preto','3')
+console.log(satanas);
